3286c8cf8fd4f5d18e4aa898d1d2e095dc6ddb2f
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-03-30 19:05:16 +0600

    Update solution.sv

--- uvm/assignments/uvm-tb-architecture_uvm-basics_sequence_sequencer_sequence-item/problem_statement_1/solution.sv
+++ uvm/assignments/uvm-tb-architecture_uvm-basics_sequence_sequencer_sequence-item/problem_statement_1/solution.sv
@@ -1,47 +1,54 @@
 
 package alu_pkg; 
-`include "uvm_macros.svh" 
-import uvm_pkg::*; 
+  `include "uvm_macros.svh" 
+  import uvm_pkg::*; 
+  
 class alu_sequencer extends uvm_sequencer#(uvm_sequence_item); 
-`uvm_component_utils(alu_sequencer) 
-function new(string name, uvm_component parent);  
-super.new(name, parent);  
-endfunction 
+  `uvm_component_utils(alu_sequencer) 
+  function new(string name, uvm_component parent);  
+    super.new(name, parent);  
+  endfunction 
 endclass 
+  
 class alu_driver extends uvm_driver#(uvm_sequence_item); 
-`uvm_component_utils(alu_driver) 
-function new(string name, uvm_component parent);  
-super.new(name, parent);  
-endfunction 
+  `uvm_component_utils(alu_driver) 
+  function new(string name, uvm_component parent);  
+    super.new(name, parent);  
+  endfunction 
 endclass 
+  
 class alu_monitor extends uvm_component; 
-`uvm_component_utils(alu_monitor) 
-function new(string name, uvm_component parent);  
-super.new(name, parent);  
-endfunction 
+  `uvm_component_utils(alu_monitor) 
+  function new(string name, uvm_component parent);  
+    super.new(name, parent);  
+  endfunction 
 endclass 
+  
 class alu_agent extends uvm_agent; 
-`uvm_component_utils(alu_agent) 
-alu_sequencer seqr; 
-alu_driver drv; 
-alu_monitor mon; 
-function new(string name = "alu_agent", uvm_component parent = null); 
-super.new(name, parent); 
-endfunction 
-function void build_phase (uvm_phase phase); 
-super.build_phase(phase); 
-if(!uvm_config_db #(uvm_active_passive_enum)::get(this, "", "is_active", is_active)) 
-is_active = UVM_ACTIVE; 
-mon = alu_monitor::type_id::create("mon", this); 
-if (is_active == UVM_ACTIVE) begin 
-seqr = alu_sequencer::type_id::create("seqr", this); 
-drv = alu_driver::type_id::create("drv", this); 
-end 
-endfunction 
-function void connect_phase(uvm_phase phase); 
-super.connect_phase(phase); 
-if(is_active == UVM_ACTIVE) 
-drv.seq_item_port.connect (seqr.seq_item_export); 
-endfunction 
+  `uvm_component_utils(alu_agent) 
+  alu_sequencer seqr; 
+  alu_driver drv; 
+  alu_monitor mon; 
+  function new(string name = "alu_agent", uvm_component parent = null); 
+    super.new(name, parent); 
+  endfunction 
+
+  function void build_phase (uvm_phase phase); 
+    super.build_phase(phase); 
+    if(!uvm_config_db #(uvm_active_passive_enum)::get(this, "", "is_active", is_active)) 
+      is_active = UVM_ACTIVE; 
+    mon = alu_monitor::type_id::create("mon", this); 
+    
+    if (is_active == UVM_ACTIVE) begin 
+      seqr = alu_sequencer::type_id::create("seqr", this); 
+      drv = alu_driver::type_id::create("drv", this); 
+    end 
+  endfunction 
+  
+  function void connect_phase(uvm_phase phase); 
+    super.connect_phase(phase); 
+    if(is_active == UVM_ACTIVE) 
+      drv.seq_item_port.connect (seqr.seq_item_export); 
+  endfunction 
 endclass 
 endpackage  
