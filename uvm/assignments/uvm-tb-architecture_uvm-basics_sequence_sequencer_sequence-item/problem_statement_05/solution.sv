fa0d4945b548a79b63dccb7a1f86b413183e12a9
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-03-31 08:42:31 +0600

    Update solution.sv

--- uvm/assignments/uvm-tb-architecture_uvm-basics_sequence_sequencer_sequence-item/problem_statement_05/solution.sv
+++ uvm/assignments/uvm-tb-architecture_uvm-basics_sequence_sequencer_sequence-item/problem_statement_05/solution.sv
@@ -1,1 +1,38 @@
+// Code your testbench here
+// or browse Examples
 
+// Test Class to Set the `delay_enable` Variable in `uvm_config_db`
+uvm_config_db#(bit)::set(this, "", "delay_enable", 1);
+
+// Driver with Delay control
+class alu_driver extends uvm_driver;
+  bit delay_enable;    // Variable to store delay_enable flag
+  
+  `uvm_component_utils(alu_driver)
+  
+  function new (string name, uvm_component parent);  
+    super.new(name, parent);  
+  endfunction
+
+  // Build Phase: Retrieve the `delay_enable` value from `uvm_config_db`
+  function void build_phase (uvm_phasephase);
+    super.build_phase(phase);
+    uvm_config_db#(bit)::get(this, "", "delay_enable", delay enable);
+  endfunction
+  
+  // Run Phase: Apply delay if `delay_enable` is 1
+  task run_phase(uvm_phase phase);  
+    alu_sequence_item pkt;
+    
+    forever begin        
+  // Get the next sequence item
+      seq_item_port.get_next_item(pkt);
+  
+  // Mark the item as done     
+      if (delay_enable) #10ns;
+      
+  // Mark the item as done
+      seq_item_port.item_done();
+    end
+  endtask
+endclass
