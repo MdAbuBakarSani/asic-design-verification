31ac6471d14768b3c309fa52b0831ad4360f40e1
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-20 12:58:28 +0600

    Update solution.sv

--- uvm/assignments/uvm-phases_active-agent_passive-agent/problem_statement_07/solution.sv
+++ uvm/assignments/uvm-phases_active-agent_passive-agent/problem_statement_07/solution.sv
@@ -1,1 +1,55 @@
+// Code your testbench here 
+// or browse Examples 
 
+`include "uvm_macros.svh" 
+import uvm_pkg::*;
+
+// === Monitor Class ===
+class bus_monitor extends uvm_monitor; 
+  `uvm_component_utils(bus_monitor) 
+
+  virtual bus_if vif;    
+  uvm_analysis_port#(bus_transaction)mon_ap; 
+
+// === Queue used to store all observed transactions until extract_phase === 
+  bus_transaction collected_transaction [$]; 
+  
+  function new (string name, uvm_component parent); 
+    super.new(name, parent); 
+    mon_ap = new("mon_ap", this); 
+  endfunction 
+
+// === Build phase to get the virtual interface from config_db ===
+  function void build_phase (uvm_phase phase); 
+    super.build_phase(phase); 
+
+// === Gets the virtual interface from the UVM configuration database ===
+    if(!uvm_config_db#(virtual bus_if)::get(this, "", "vif", vif))  
+      `uvm_fatal("No Virtual Interface", "Monitor") 
+      endfunction 
+
+// === Run phase continuously observes bus activity ===
+  task run_phase(uvm_phase phase); 
+    bus_transaction tr; 
+
+    forever begin 
+      @(posedge vif.clk); 
+
+      .................. 
+      ..................  
+      .................. 
+
+      collected_transaction.push_back(tr); 
+      mon_ap.write(tr);
+    end 
+  endtask 
+
+// === Extract Phase ===
+    function void extract_phase (uvm_phase phase); 
+      super.extract_phase(phase); 
+
+      foreach (collected_transaction[i]) begin 
+        $display("Monitor => Extract Phase: %p", collected_transaction[i]); 
+      end 
+  endfunction 
+endclass
