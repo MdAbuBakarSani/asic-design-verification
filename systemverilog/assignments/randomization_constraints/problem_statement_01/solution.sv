0f0a77b46f23194864c9abf86c9b299d417aa803
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-21 21:16:32 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_01/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_01/solution.sv
@@ -1,1 +1,35 @@
+class DivisibleBy11;
+  rand int number;
 
+// === Constraint block ===
+  constraint range {
+    number inside {[1000:9999]};
+    number % 11 == 0;
+  }
+
+// === Method to randomize and display the value ===
+  function void display();
+
+    if (!this.randomize())
+      $display("Randomization failed!");
+    else
+      $display("Random 4-digit number divisible by 11 : %0d", number);
+
+  endfunction
+endclass
+
+
+// === Testbench ===
+
+module tb_divisible_by_11;
+  DivisibleBy11 num;
+
+  initial begin
+    num = new();
+
+// === Generate 10 random values ===
+    repeat (10) begin
+      num.display();
+    end
+  end
+endmodule
