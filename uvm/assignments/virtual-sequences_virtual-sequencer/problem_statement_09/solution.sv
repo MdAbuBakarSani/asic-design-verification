f7f8de64e1ad956dfd2edd03c9d56ddc25d4669f
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-21 17:20:24 +0600

    Update solution.sv

--- uvm/assignments/virtual-sequences_virtual-sequencer/problem_statement_09/solution.sv
+++ uvm/assignments/virtual-sequences_virtual-sequencer/problem_statement_09/solution.sv
@@ -1,1 +1,43 @@
+// Code your testbench here
+// or browse Examples
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class ordered_virtual_sequence extends uvm_sequence#(uvm_sequence_item);
+  `uvm_object_utils(ordered_virtual_sequence)
+  `uvm_declare_p_sequencer(virtual_sequencer)
+
+  function new(string n="ordered_virtual_sequence"); 
+    super.new(n); 
+  endfunction
+
+  task body();
+    seq_A a = seq_A::type_id::create("a");
+    seq_B b = seq_B::type_id::create("b");
+    seq_C c = seq_C::type_id::create("c");
+
+    `uvm_info("VSEQ","Start A on READ",  UVM_LOW)
+    a.start(p_sequencer.read_sqr);    
+    `uvm_info("VSEQ","Finished",  UVM_LOW)
+
+    `uvm_info("VSEQ","Start B on WRITE", UVM_LOW)
+    b.start(p_sequencer.write_sqr);   
+    `uvm_info("VSEQ","Finished",  UVM_LOW)
+
+    `uvm_info("VSEQ","Start C on READ",  UVM_LOW)
+    c.start(p_sequencer.read_sqr);   
+    `uvm_info("VSEQ","Finished",  UVM_LOW)
+  endtask
+endclass
+
+
+
+
+
+
+
+
+
+
 
