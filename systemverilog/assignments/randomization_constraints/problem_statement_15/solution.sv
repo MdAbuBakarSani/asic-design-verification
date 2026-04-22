84f1079941afe525e5b0f161a73985ef2815be11
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 16:51:45 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_15/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_15/solution.sv
@@ -1,1 +1,33 @@
+// Code your testbench here 
+// or browse Examples 
 
+class TwoOnes;
+
+  // Random 4-bit number
+  rand bit [3:0] number;
+
+  // Constraint block
+  constraint rules{
+    $countones(number) == 2;    // Constraint: exactly two bits must be 1
+  }
+
+  // Method to randomize and display value
+  function void display();
+    if (!this.randomize())    
+      $display("Randomization failed!");
+    else
+      $display("Random 4-bit number with exactly 2 ones: %0b (%0d)", number, number);
+  endfunction
+endclass
+
+// Testbench
+module tb_two_ones;
+  TwoOnes value;
+  initial begin
+    value = new();
+    // Generate 5 random values
+    repeat(5) begin
+      value.display();
+    end
+  end
+endmodule
