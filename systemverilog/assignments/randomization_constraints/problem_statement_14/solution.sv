04cbc5bb5bd36c73d840d5e8806a0dd200adee70
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 16:43:22 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_14/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_14/solution.sv
@@ -1,37 +1,44 @@
-
 // Code your testbench here 
 // or browse Examples 
 
-
 class NonsequentialDigitPin;
-rand bit [3:0] pin [4];
 
-constraint range{
-    foreach (pin[i]) pin[i] inside {[0:9]};
-}
-constraint no_sequential{
-    !(pin[1] == pin[0] +1 && pin[2] == pin[1] + 1 && pin[3] == pin[2] + 1);
-    !(pin[1] == pin[0] -1 && pin[2] == pin[1] - 1 && pin[3] == pin[2] - 1);
-}
-function void display();
-    if (!this.randomize())
-        $display("Randomization failed!");
-    else begin
-        $write("Random 4-digit pin (non-sequential): ");
-        foreach (pin[i])
-        $write("%0d", pin[i]);
-        $display("");
-    end
-endfunction
+    // Random array of 4 digits
+    rand bit [3:0] pin [4];
+
+    // Constraint block 
+    constraint range{
+        foreach (pin[i]) pin[i] inside {[0:9]};    //each digit must be between 0 and 9
+    }
+
+    // Constraint: prevent ascending or descending sequential patterns
+    constraint no_sequential{
+        !(pin[1] == pin[0] +1 && pin[2] == pin[1] + 1 && pin[3] == pin[2] + 1);
+        
+        !(pin[1] == pin[0] -1 && pin[2] == pin[1] - 1 && pin[3] == pin[2] - 1);
+    }
+
+    // Method to randomize and display the PIN
+    function void display();
+        if (!this.randomize())
+            $display("Randomization failed!");
+        else begin
+            $write("Random 4-digit pin (non-sequential): ");
+            foreach (pin[i])
+                $write("%0d", pin[i]);
+            $display("");
+        end
+    endfunction
 endclass
 
+// Testbench
 module tb_nonsequential_digit_pin;
-NonsequentialDigitPin value;
+    NonsequentialDigitPin value;
     initial begin
-      value = new();
-      repeat(5) begin
-      value.display();
+        value = new();
+        // Generate 5 random PINs
+        repeat(5) begin
+            value.display();
+        end
     end
-  end
-endmodule
-     
+endmodule   
