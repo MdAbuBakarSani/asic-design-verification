d0c7878838723444fe1b074d831ac48d17aa2e88
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 13:12:05 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_05/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_05/solution.sv
@@ -1,1 +1,42 @@
+// Code your testbench here 
+// or browse Examples 
 
+class Arrays;
+
+  // Random array of 10 integers
+  rand int arr[10];
+
+  int total;
+
+  // Constraint block
+  constraint elements {
+    foreach (arr[i]) arr[i] inside {[1:50]};      // Each element between 1 and 50
+    (arr.sum()) < 300;                            // Sum of array elements less than 300
+  }
+
+  // Method to randomize and display values
+  function void display();  
+    if (!this.randomize())   
+      $display ("Randomization failed!");
+    else begin   
+      total = arr.sum();
+      $display ("Random array of 10 integers between 1 and 50: ");
+      foreach (arr[i])
+        $write ("%0d, ", arr[i]);
+      $display("");
+      $display("Total sum (less than 300) = %0d", total);
+    end   
+  endfunction
+endclass
+
+// Testbench
+module tb_array;
+  Arrays obj; 
+  initial begin  
+    obj = new(); 
+    // Generate 5 different arrays
+    repeat(5) begin
+      obj.display();
+    end
+  end
+endmodule
