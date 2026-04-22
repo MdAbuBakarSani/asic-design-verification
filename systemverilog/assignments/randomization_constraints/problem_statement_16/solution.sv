5cfab74fd3c6d37c2e03e375cc10819d7128d6be
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 16:56:42 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_16/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_16/solution.sv
@@ -1,1 +1,55 @@
+// Code your testbench here 
+// or browse Examples
 
+
+
+class Array;
+
+  rand int arr[5];
+
+  constraint range{
+    
+    foreach (arr[i]) arr[i] inside {[21:79]};
+
+  }
+
+  function void display();
+   
+    if (!this.randomize())
+      
+      $display("Randomization failed!");
+    
+    else begin
+      
+      $display("Randomized array with elements greater than 20 and less than 80: ");
+       
+      foreach (arr[i])
+      
+        $write("%0d ", arr[i]);
+      
+      $display("");
+   
+    end
+
+  endfunction
+
+endclass
+
+
+module tb_array;
+
+  Array elements;
+  
+  initial begin
+    
+    elements = new();
+     
+    repeat(5) begin
+      
+      elements.display();
+      
+    end
+    
+  end
+
+endmodule
