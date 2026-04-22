b4203d53fea81516e98a6a386af6995d9f0a6362
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 19:43:50 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_23/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_23/solution.sv
@@ -1,1 +1,36 @@
+// Code your testbench here 
+// or browse Examples 
 
+class GrayCode;
+
+  // Random binary input
+  rand bit [4:0] binary_Input;
+
+  // Gray code output
+  bit [4:0] gray_code; 
+
+// Post-randomization: convert binary to Gray code
+  function void post_randomize();
+    gray_code = binary_Input ^ (binary_Input >> 1);
+  endfunction
+
+  // Method to randomize and display values
+  function void display();
+    if (!this.randomize())
+      $display("Randomization failed!");
+    else
+      $display("Binary: %5b | Gray Code: %5b", binary_Input, gray_code);
+  endfunction
+endclass
+
+// Testbench
+module tb;
+  GrayCode digits;
+  initial begin
+    digits = new();
+    // Generate 5 random values
+    repeat(5) begin
+      digits.display();
+    end
+  end
+endmodule
