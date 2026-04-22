756c9131f8c39aaccef23682b5bf951ac24b065b
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 16:04:22 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_13/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_13/solution.sv
@@ -1,1 +1,41 @@
+// Code your testbench here 
+// or browse Examples 
 
+class IPAddress;
+
+  // Random 8-bit segments
+  rand bit [7:0] seg1, seg2, seg3, seg4;
+
+  // Constraint block
+  constraint ip_range {
+   
+    // First byte constraint (exclude 127)
+    seg1 inside {[1:255]};
+    seg1 != 127;
+   
+    // Remaining bytes (0–255)
+    seg2 inside {[0:255]};
+    seg3 inside {[0:255]};
+    seg4 inside {[0:255]};
+  }
+
+  // Method to randomize and display IP
+  function void display ();
+    if (!this.randomize())
+      $display ("Randomization failed!");
+    else
+      $display ("Random IP Address : %0d.%0d.%0d.%0d", seg1, seg2, seg3, seg4);
+  endfunction
+endclass
+
+// Testbench
+module tb_ip_address;
+  IPAddress ip;
+  initial begin
+    ip = new();
+    // Generate 5 random IP addresses
+    repeat(5) begin
+      ip.display();
+    end
+  end
+endmodule
