1bdde682338db18fc3b070f182acd76da7aeab52
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-22 15:47:51 +0600

    Update solution.sv

--- systemverilog/assignments/randomization_constraints/problem_statement_12/solution.sv
+++ systemverilog/assignments/randomization_constraints/problem_statement_12/solution.sv
@@ -1,1 +1,38 @@
+// Code your testbench here 
+// or browse Examples 
 
+class Time24HourFormat;
+ 
+  // Random variables
+  rand int hour, minute;
+
+  // Constraint block
+  constraint range {
+    
+    // Valid hour range (24-hour format)
+    hour inside {[0:23]};
+  
+    // Valid minute range
+    minute inside {[0:59]};   
+  }
+
+  // Method to randomize and display time
+  function void display();
+    if(!this.randomize()) 
+      $display("Randomization failed!");
+    else
+      $display("Random Time (24-hour format): %02d:%02d", hour, minute);
+  endfunction
+endclass
+
+// Testbench
+module tb_time_24_hour_format;
+  Time24HourFormat obj;
+  initial begin
+    obj = new();
+    // Generate 5 random times
+    repeat(5) begin
+      obj.display(); 
+    end 
+  end
+endmodule
