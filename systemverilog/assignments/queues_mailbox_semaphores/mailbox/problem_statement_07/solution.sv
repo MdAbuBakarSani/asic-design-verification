d6563689206e97e2ac7e39e3391f1c3482293cae
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-23 14:43:15 +0600

    Fix typo in display messages for mailbox operations

--- systemverilog/assignments/queues_mailbox_semaphores/mailbox/problem_statement_07/solution.sv
+++ systemverilog/assignments/queues_mailbox_semaphores/mailbox/problem_statement_07/solution.sv
@@ -23,11 +23,11 @@ module mailbox_Method_demo;
     
     // Blocking get
     mb.get(value1);
-    $display("Recieved data: %0d | Count= %0d", value1, mb.num());
+    $display("Received data: %0d | Count= %0d", value1, mb.num());
     
     // Non-blocking get
     if(mb.try_get(value2))
-      $display("try_get(): %0d successfull | Count= %0d", value2, mb.num());
+      $display("try_get(): %0d successful | Count= %0d", value2, mb.num());
     else
       $display("try_get() failed - mailbox empty.");
   end
