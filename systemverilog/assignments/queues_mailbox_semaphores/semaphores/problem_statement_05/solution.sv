38e934ab0c7a4f5bf48f0d746ac3c942fde964f1
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-23 16:50:02 +0600

    Update solution.sv

--- systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_05/solution.sv
+++ systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_05/solution.sv
@@ -3,67 +3,49 @@
 
 module semaphore_wait_resources_demo;
  
+ // Semaphore with 4 available keys
   semaphore sema = new(4);
-  
  
+ // Function to define how many keys each procedure needs
   function int id_serial(int id);
-    
     case (id)
-      
       1: return 3;
-     
       2: return 2;
-     
       3: return 1;
-    
       4: return 2;
-     
       5: return 1;
-     
       default: return 1;
-    
     endcase
-  
   endfunction
   
  
+ // Task representing a process waiting for resources
   task automatic wait_resources (string name, int id);
    
     int ID = id_serial(id);
    
     $display("%s [%0d] requests %0d key(s) at %0t ns.", name, id, ID, $time);
    
-    sema.get(ID);
+   sema.get(ID);    
    
     $display("%s [%0d] ACQUIRED %0d at %0t ns.", name, id, ID, $time);
    
     #10;
-   
-    sema.put(ID);
+
+   sema.put(ID);              
    
     $display("%s [%0d] RELEASED %0d at %0t ns.", name, id, ID, $time);
-  
   endtask
   
   
   initial begin
     
     fork
-     
       wait_resources ("Procedure", 1);
-     
       wait_resources ("Procedure", 2);
-     
       wait_resources ("Procedure", 3);
-    
       wait_resources ("Procedure", 4);
-     
       wait_resources ("Procedure", 5);
-   
     join
-
   end
-
 endmodule
-   
-
