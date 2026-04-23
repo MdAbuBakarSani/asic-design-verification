542d4a2b2ee66dcc766689b5affb8d08cd670b80
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-23 16:03:23 +0600

    Update solution.sv

--- systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_05/solution.sv
+++ systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_05/solution.sv
@@ -1,1 +1,69 @@
+// Code your testbench here 
+// or browse Examples 
+
+module semaphore_wait_resources_demo;
+ 
+  semaphore sema = new(4);
+  
+ 
+  function int id_serial(int id);
+    
+    case (id)
+      
+      1: return 3;
+     
+      2: return 2;
+     
+      3: return 1;
+    
+      4: return 2;
+     
+      5: return 1;
+     
+      default: return 1;
+    
+    endcase
+  
+  endfunction
+  
+ 
+  task automatic wait_resources (string name, int id);
+   
+    int ID = id_serial(id);
+   
+    $display("%s [%0d] requests %0d key(s) at %0t ns.", name, id, ID, $time);
+   
+    sema.get(ID);
+   
+    $display("%s [%0d] ACQUIRED %0d at %0t ns.", name, id, ID, $time);
+   
+    #10;
+   
+    sema.put(ID);
+   
+    $display("%s [%0d] RELEASED %0d at %0t ns.", name, id, ID, $time);
+  
+  endtask
+  
+  
+  initial begin
+    
+    fork
+     
+      wait_resources ("Procedure", 1);
+     
+      wait_resources ("Procedure", 2);
+     
+      wait_resources ("Procedure", 3);
+    
+      wait_resources ("Procedure", 4);
+     
+      wait_resources ("Procedure", 5);
+   
+    join
+
+  end
+
+endmodule
+   
 
