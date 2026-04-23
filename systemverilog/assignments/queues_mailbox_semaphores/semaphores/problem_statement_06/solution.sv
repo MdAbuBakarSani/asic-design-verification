afea24b01493bd9c75f9ea71b188c888ce4abe2e
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-23 17:00:22 +0600

    Update solution.sv

--- systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_06/solution.sv
+++ systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_06/solution.sv
@@ -1,1 +1,43 @@
+// Code your testbench here 
+// or browse Examples 
 
+module semaphore_synchronization_mechanism_demo;
+  
+  // Semaphore initialized with 0 keys 
+  semaphore sema = new(0);
+  
+
+  // First process: sends signal
+  task mechanism1(string name);
+    $display("%s getting ready at %0tns.", name, $time);
+ 
+    #10;
+     
+    sema.put(1);       
+    $display("%s send signal at %0tns.", name, $time);
+  endtask
+    
+ 
+  // Second process: waits for signal
+  task mechanism2(string name);
+    
+    $display("%s waiting....", name);
+    sema.get(1);        // Wait until signal is available
+  
+    #10;
+    $display("%s recieved signal at %0tns.", name, $time);
+ 
+    #10;
+    $display("%s finished at %0tns.", name, $time); 
+  endtask
+  
+  
+  initial begin
+   
+    // Run both processes in parallel
+    fork
+      mechanism1 ("Mechanism 1");
+      mechanism2 ("Mechanism 2");
+    join
+  end
+endmodule
