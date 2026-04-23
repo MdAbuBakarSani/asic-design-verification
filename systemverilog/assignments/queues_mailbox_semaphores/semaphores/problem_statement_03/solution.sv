abebf1920641aa02d98c039b7d22fcb2dd1c9c05
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-23 15:50:08 +0600

    Update solution.sv

--- systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_03/solution.sv
+++ systemverilog/assignments/queues_mailbox_semaphores/semaphores/problem_statement_03/solution.sv
@@ -1,1 +1,35 @@
+// Code your testbench here 
+// or browse Examples 
 
+module semaphore_shared_resource_demo;
+ 
+  // Semaphore with 1 key for exclusive access
+  semaphore sema = new(1);
+  
+  // Task that accesses shared resource
+  task automatic shared_resource (string name);
+   
+    int id = 1;
+    
+    sema.get(1);    // Acquire resource
+    $display("%s: Task %0d start at %0tns.", name, id, $time);
+    
+    #10;
+  
+    sema.put(1); // Release resource
+    $display("%s: Task %0d end at %0tns.", name, id, $time);
+  endtask
+  
+ 
+  initial begin
+    
+    // Multiple tasks try to access the same resource
+    fork
+      shared_resource ("Resource 1");
+      shared_resource ("Resource 2");
+      shared_resource ("Resource 3");
+      shared_resource ("Resource 4");
+      shared_resource ("Resource 5");
+    join 
+  end
+endmodule
