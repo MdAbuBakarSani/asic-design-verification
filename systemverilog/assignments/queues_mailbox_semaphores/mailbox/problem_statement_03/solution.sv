5b982448e6c52aafd7f0ca556df660aee360511c
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-23 12:08:44 +0600

    Update solution.sv

--- systemverilog/assignments/queues_mailbox_semaphores/mailbox/problem_statement_03/solution.sv
+++ systemverilog/assignments/queues_mailbox_semaphores/mailbox/problem_statement_03/solution.sv
@@ -1,1 +1,50 @@
+// Code your testbench here 
+// or browse Examples 
 
+module mailbox_producer_consumer_model;
+ 
+  // Mailbox with capacity 3
+  mailbox mb = new(3);
+  
+  // Producer task
+  task producer();
+  
+    int i;
+   
+    for (i = 1; i <=5; i++) begin
+      $display ("[Producer] Trying to send: %0d", i);
+    
+      mb.put(i);     // Blocking if mailbox is full   
+      $display ("[Producer] Sent: %0d  | Mailbox count: %0d", i, mb.num());
+    
+      #2;            // Delay to simulate production time  
+    end
+  endtask
+
+  
+  // Consumer task
+  task consumer();
+   
+    int data;
+   
+    repeat(5) begin 
+      #7;               // Delay to simulate consumption time
+     
+      mb.get(data);     // Blocking if mailbox is empty
+      $display ("[Consumer] Recieved: %0d  | Mailbox count: %0d", data, mb.num());   
+    end
+  endtask
+  
+ 
+  initial begin
+    $display ("Starting Producer-Consumer Mailbox Model Simulation: ");
+    
+    // Run producer and consumer in parallel
+    fork
+      producer();
+      consumer();
+    join
+   
+    $display ("Simulation Successfully complete.");
+  end
+endmodule
