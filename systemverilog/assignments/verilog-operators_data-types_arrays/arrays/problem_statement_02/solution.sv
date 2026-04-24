28161d3df2eb2a4f25c0bf1699aec7de85d41368
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-24 11:01:38 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_02/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_02/solution.sv
@@ -1,1 +1,25 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Packed_Union_Example;
+
+  // Define packed union
+ 
+  typedef union packed {
+    bit [7:0] data_value;
+  } data_union;
+
+  // Declare instance
+  data_union data;
+
+  initial begin
+
+    // Initialize value
+    data.data_value = 8'b10110101;
+
+    // Display result
+    $display("Packed Union Elements:");
+    $display("data_value = %b", data.data_value);
+
+  end
+endmodule
