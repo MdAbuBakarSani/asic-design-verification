f050016616c75bfce2a1b5fee3aa8fe5837c5394
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-24 13:59:42 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_10/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_10/solution.sv
@@ -1,1 +1,29 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Array_Ordering_Methods;
+
+  int arr[8] = '{76, 26, 40, 13, 8, 66, 84, 57};
+
+  initial begin
+
+    $display("Original Statements: %p", arr);
+
+    // reverse() – reverses the array
+    arr.reverse();
+    $display("After reverse : %p", arr);
+
+    // sort() – sorts in ascending order
+    arr.sort();
+    $display("After sort : %p", arr);
+
+    // rsort() – sorts in descending order
+    arr.rsort();
+    $display("After rsort : %p", arr);
+
+    // shuffle() – randomizes the order
+    arr.shuffle();
+    $display("After shuffle : %p", arr);
+
+  end
+endmodule
