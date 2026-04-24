8214c5c0e7b60b4ce4320c3b38ca0141e6cf8249
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-24 12:11:08 +0600

    Add dynamic array example in SystemVerilog
    Implemented a dynamic array example in SystemVerilog, including memory allocation, value assignment, element display, and deletion of the array.

--- systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_07/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_07/solution.sv
@@ -1,1 +1,31 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Dynamic_Array_Example;
+
+  int arr[];
+
+  initial begin
+
+    // Allocate memory
+    arr = new[5];
+
+    // Assign values
+    arr = '{5, 10, 15, 20, 25};
+
+    // Display elements
+    $display("Array Elements:");
+    foreach (arr[i])
+      $display("Array[%0d] = %0d", i, arr[i]);
+
+    // Display size
+    $display("Size of the array is: %0d", arr.size());
+
+    // Delete array
+    arr.delete();
+
+    // Display size after delete
+    $display("Size of the array after delete: %0d", arr.size());
+
+  end
+endmodule
