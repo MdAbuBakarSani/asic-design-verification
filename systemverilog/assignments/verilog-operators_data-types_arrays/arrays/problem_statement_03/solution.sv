fcb7ed0a5911271c78985d0a7b93bd256c21c47c
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-24 11:07:41 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_03/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/arrays/problem_statement_03/solution.sv
@@ -1,1 +1,18 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Fixed_Array;
+
+  // Fixed-size array with initialization
+  int arr[4] = '{10, 20, 30, 40};
+
+  initial begin
+
+    $display("Fixed-size Array Elements:");
+
+    // Display elements
+    foreach (arr[i])
+      $display("arr[%0d] = %0d", i, arr[i]);
+
+  end
+endmodule
