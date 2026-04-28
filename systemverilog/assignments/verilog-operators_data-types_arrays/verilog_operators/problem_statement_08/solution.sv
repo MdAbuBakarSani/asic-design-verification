b663838c816d1c89a883ecda404e866f88253756
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-28 12:42:56 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_08/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_08/solution.sv
@@ -1,1 +1,18 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Replication_Operator;
+
+  logic [3:0] a = 4'b1010;
+  logic [11:0] replicated_value;
+
+  initial begin
+
+    $display("Replication Operator in SystemVerilog");
+    $display("Original value a = %b", a);
+
+    replicated_value = {3{a}};
+    $display("Replicated value {3{a}} = %b", replicated_value);
+
+  end
+endmodule
