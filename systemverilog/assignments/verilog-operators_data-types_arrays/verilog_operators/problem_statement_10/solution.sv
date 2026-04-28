6054a34bdb333353e3812bffea7656070f58a3a8
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-28 12:57:52 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_10/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_10/solution.sv
@@ -1,1 +1,18 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Conditional_Operator;
+
+  int a = 55;
+  int b = 87;
+  string result;
+
+  initial begin
+
+    $display("Conditional Operator in SystemVerilog");
+
+    result = (a > b) ? "a is greater" : "b is greater";
+    $display("Result is : %s", result);
+
+  end
+endmodule
