ae69152ebf247505a48d645b96ac56febedad60d
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-28 12:38:22 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_07/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_07/solution.sv
@@ -1,1 +1,18 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Concatenation_Operator;
+
+  logic [3:0] a = 4'b1010;
+  logic [3:0] b = 4'b1100;
+  logic [7:0] result_concat;
+
+  initial begin
+
+    $display("Concatenation Operator in SystemVerilog");
+
+    result_concat = {a, b};
+    $display("Concatenation of a and b: {%b, %b} = %b", a, b, result_concat);
+
+  end
+endmodule
