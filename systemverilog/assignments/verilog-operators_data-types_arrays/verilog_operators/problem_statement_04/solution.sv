0b8cddb4a2b93fc3531cf2ed1e1e1a72367e13ae
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-28 12:19:18 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_04/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_04/solution.sv
@@ -1,1 +1,38 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Reduction_Operators;
+
+  logic [3:0] a = 4'b1101;
+  logic result;
+
+  initial begin
+
+    $display("Reduction Operators in SystemVerilog");
+
+    // AND reduction
+    result = &a;
+    $display("&a = &%b = %0b", a, result);
+
+    // NAND reduction
+    result = ~&a;
+    $display("~&a = ~&%b = %0b", a, result);
+
+    // OR reduction
+    result = |a;
+    $display("|a = |%b = %0b", a, result);
+
+    // NOR reduction
+    result = ~|a;
+    $display("~|a = ~|%b = %0b", a, result);
+
+    // XOR reduction
+    result = ^a;
+    $display("^a = ^%b = %0b", a, result);
+
+    // XNOR reduction
+    result = ~^a;
+    $display("~^a = ~^%b = %0b", a, result);
+
+  end
+endmodule
