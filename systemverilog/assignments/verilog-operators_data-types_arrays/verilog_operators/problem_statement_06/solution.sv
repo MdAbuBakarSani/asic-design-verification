5cd27e157d1406906900c07bbe67e1fafceef069
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-28 12:32:38 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_06/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/verilog_operators/problem_statement_06/solution.sv
@@ -1,1 +1,30 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Increment_Decrement_Operators;
+
+  int a = 7;
+
+  initial begin
+
+    $display("Increment Decrement Operators in SystemVerilog");
+    $display("Initial value: a = %0d", a);
+
+    // Post-Increment
+    a++;
+    $display("After Post-Increment : a = %0d", a);
+
+    // Pre-Increment
+    ++a;
+    $display("After Pre-Increment : a = %0d", a);
+
+    // Post-Decrement
+    a--;
+    $display("After Post-Decrement : a = %0d", a);
+
+    // Pre-Decrement
+    --a;
+    $display("After Pre-Decrement : a = %0d", a);
+
+  end
+endmodule
