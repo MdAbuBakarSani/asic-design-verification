ae19fd58c02c7199d51b35cc0a001a8d359f7a41
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 17:30:28 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_11/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_11/solution.sv
@@ -1,1 +1,15 @@
+// Code your testbench here 
+// or browse Examples 
 
+module LowerCase_Example;
+
+  string message = "I AM A TRAINEE AT PROV LOGIC";
+
+  initial begin
+
+    $display("Original string: %s", message);
+
+    $display("LowerCase String: %s", message.tolower());
+
+  end
+endmodule
