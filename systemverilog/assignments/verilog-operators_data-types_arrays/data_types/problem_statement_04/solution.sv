320171b51b84456e249e0adee742b4cd9f547cf7
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 14:04:56 +0600

    Add module for testing signed data types in SystemVerilog

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_04/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_04/solution.sv
@@ -1,1 +1,23 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Signed_Data_Type;
+
+  byte      a = 8'd101;
+  shortint  b = -16'd2500;
+  int       c = -32'd30000;
+  integer   d = 32'd12345;
+  longint   e = 64'd650123;
+
+  initial begin
+
+    $display("SystemVerilog Signed Data Types:");
+
+    $display("byte a = %0d", a);
+    $display("shortint b = %0d", b);
+    $display("int c = %0d", c);
+    $display("integer d = %0d", d);
+    $display("longint e = %0d", e);
+
+  end
+endmodule
