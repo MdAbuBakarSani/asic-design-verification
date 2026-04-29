40f61689a1873b62d7f77490c20bc9f0375a7952
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 17:21:23 +0600

    Implement letter replacement in string

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_09/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_09/solution.sv
@@ -1,1 +1,29 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Replace_Letters;
+
+  string original = "systemverilog";
+  string modified = "";
+  byte ch;
+
+  initial begin
+
+    for (int i = 0; i < original.len(); i++) begin
+      ch = original[i];
+
+      if (ch == "s")
+        ch = "S";
+      else if (ch == "v")
+        ch = "V";
+      else if (ch == "t")
+        ch = "T";
+
+      modified = {modified, ch};
+    end
+
+    $display("Original string: %s", original);
+    $display("Modified string: %s", modified);
+
+  end
+endmodule
