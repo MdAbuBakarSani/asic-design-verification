8274247c108b93d463fb6949997514174ffe5a52
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 12:59:31 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_02/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_02/solution.sv
@@ -1,1 +1,30 @@
+// Code your testbench here 
+// or browse Examples
 
+module Blocking_NonBlocking_Example;
+
+  reg a, b, c;
+  reg x, y, z;
+
+  initial begin
+
+    // Blocking assignment
+    a = 1;
+    b = a;
+    c = b;
+
+    $display("Blocking Assignments:");
+    $display("a = %b, b = %b, c = %b", a, b, c);
+
+    // Non-blocking assignment
+    #1;
+    x <= 1;
+    y <= x;
+    z <= y;
+
+    #1;
+    $display("Non-Blocking Assignments:");
+    $display("x = %b, y = %b, z = %b", x, y, z);
+
+  end
+endmodule
