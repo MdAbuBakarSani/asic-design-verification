9beb647e0ea6285f1d843191b8d26e7eb34c94d3
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 12:52:13 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_01/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_01/solution.sv
@@ -1,1 +1,19 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Continuous_Assignment_Example;
+
+  wire a, b, xor_result;
+
+  // Continuous assignments
+  assign a = 1'b1;
+  assign b = 1'b0;
+  assign xor_result = a ^ b;
+
+  initial begin
+    #1;
+    $display("Continuous_Assignment_Example");
+    $display("Inputs: a = %b, b = %b", a, b);
+    $display("Output: a ^ b = %b", xor_result);
+  end
+endmodule
