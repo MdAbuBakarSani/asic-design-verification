2097bb40f1da6b95b72325a232eb63ac6e788ce8
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 13:07:25 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_03/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_03/solution.sv
@@ -1,1 +1,32 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Logic_Assignments;
+
+  logic a, b;
+  logic blocking_assignment;
+  logic nonblocking_assignment;
+
+  // Continuous assignment
+  assign a = 1'b1;
+  assign b = 1'b0;
+
+  initial begin
+
+    $display("Continuous, Blocking and Non-blocking Assignments in SystemVerilog");
+    $display("Input Values: a = %b, b = %b", a, b);
+
+    // Blocking Assignment
+    blocking_assignment = a | b;
+    $display("Blocking Assignment : a | b = %b", blocking_assignment);
+
+    #1;
+
+    // Non-blocking Assignment
+    nonblocking_assignment <= a ^ b;
+
+    #1;
+    $display("NonBlocking Assignment : a ^ b = %b", nonblocking_assignment);
+
+  end
+endmodule
