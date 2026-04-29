b8fad490d77229924269482d8a1d017883820161
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 16:59:03 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_08/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_08/solution.sv
@@ -1,1 +1,18 @@
+// Code your testbench here 
+// or browse Examples 
 
+module String_Length;
+
+  string message = "SystemVerilog";
+  int length;
+
+  initial begin
+
+    $display("The message is : %s", message);
+
+    length = message.len();
+
+    $display("Length of the string : %0d", length);
+
+  end
+endmodule
