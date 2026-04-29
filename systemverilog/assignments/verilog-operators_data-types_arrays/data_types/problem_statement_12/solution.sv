d098ccbb04d3731ae839611727bfa7238d3c4126
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 17:34:33 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_12/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_12/solution.sv
@@ -1,1 +1,26 @@
+// Code your testbench here 
+// or browse Examples 
 
+module String_Compare;
+
+  string str1 = "Verilog";
+  string str2 = "SystemVerilog";
+  int result;
+
+  initial begin
+
+    $display("String 1: %s", str1);
+    $display("String 2: %s", str2);
+    $display("Comparing between String 1 and String 2 --->");
+
+    result = str1.compare(str2);
+
+    if (result == 0)
+      $display("Result: String 1 and String 2 are equal");
+    else if (result < 0)
+      $display("Result: String 1 is less than String 2");
+    else
+      $display("Result: String 1 is greater than String 2");
+
+  end
+endmodule
