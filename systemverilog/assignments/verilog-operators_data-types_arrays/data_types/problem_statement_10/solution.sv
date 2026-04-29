0af2573c15c0c940696c53332a24b4d1823cd1c3
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 17:26:27 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_10/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_10/solution.sv
@@ -1,1 +1,15 @@
+// Code your testbench here 
+// or browse Examples 
 
+module UpperCase_Example;
+
+  string message = "I am a trainee at ProV Logic";
+
+  initial begin
+    
+    $display("Original string: %s", message);
+    
+    $display("Uppercase String: %s", message.toupper());
+    
+  end
+endmodule
