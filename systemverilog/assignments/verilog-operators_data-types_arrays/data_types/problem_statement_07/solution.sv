043908501b3f481e09f1d6da6c17fd27a73e4ee6
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-29 16:48:03 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_07/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_07/solution.sv
@@ -1,1 +1,12 @@
+// Code your testbench here 
+// or browse Examples 
 
+module String_Display;
+
+  string message = "I am learning SV data type";
+
+  initial begin
+    $display("%s", message);
+  end
+
+endmodule
