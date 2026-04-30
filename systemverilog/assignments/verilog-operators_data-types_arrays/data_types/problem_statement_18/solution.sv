131c7a4871762364689346699a979e1eb175877f
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-30 11:13:17 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_18/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_18/solution.sv
@@ -1,1 +1,14 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Enum_Num_Method;
+
+  typedef enum {Verilog, SystemVerilog, UVM, Protocols, Soc} asic_dv;
+
+  initial begin
+    asic_dv course;
+
+    $display("Total Number of Courses in ASIC DV Curriculum: %0d", course.num());
+ 
+  end
+endmodule
