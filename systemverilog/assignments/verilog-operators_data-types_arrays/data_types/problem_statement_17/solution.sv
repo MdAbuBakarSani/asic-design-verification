8e63a08cc75478c9b8ff4b0c44d4dae289f64281
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-30 11:09:15 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_17/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_17/solution.sv
@@ -1,1 +1,22 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Enum_Prev_Method;
+
+  typedef enum {Verilog, SystemVerilog, UVM, Protocols, Soc} asic_dv;
+
+  int n;
+
+  initial begin
+
+    asic_dv course;
+
+    course = Protocols;
+    n = 2;
+
+    $display("Current Member: %s", course.name());
+
+    $display("nth Previous Member (n = %0d): %s", n, course.prev(n).name());
+
+  end
+endmodule
