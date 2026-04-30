420b013cac03cff4f827f466a691b87a4d1695c4
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-30 11:17:32 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_19/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_19/solution.sv
@@ -1,1 +1,17 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Enum_Name_Method;
+
+  typedef enum {Verilog, SystemVerilog, UVM, Protocols, Soc} asic_dv;
+
+  initial begin
+    asic_dv course;
+
+    course = Soc;
+
+    $display("Current Enumeration value (index): %0d", course);
+    $display("Current Name of the Enumeration value: %s", course.name());
+    
+  end
+endmodule
