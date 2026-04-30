816cef62842d56ea4aceb82363b4d7e33b03a0d4
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-04-30 11:23:08 +0600

    Update solution.sv

--- systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_20/solution.sv
+++ systemverilog/assignments/verilog-operators_data-types_arrays/data_types/problem_statement_20/solution.sv
@@ -1,1 +1,33 @@
+// Code your testbench here 
+// or browse Examples 
 
+module Const_Parameter_Example #(
+  parameter int WIDTH  = 8,
+  parameter int HEIGHT = 4
+);
+
+  int area, volume;
+
+  initial begin
+
+    // Constant values (runtime constants)
+    const int LENGTH = 8;
+    const int DEPTH  = 3;
+
+    // Calculations
+    area   = WIDTH * HEIGHT;
+    volume = LENGTH * DEPTH * HEIGHT;
+
+    // Display
+    $display("Parameter WIDTH  : %0d", WIDTH);
+    $display("Parameter HEIGHT : %0d", HEIGHT);
+
+    $display("Constant LENGTH  : %0d", LENGTH);
+    $display("Constant DEPTH   : %0d", DEPTH);
+
+    $display("Calculated area (WIDTH * HEIGHT) = (%0d * %0d) = %0d", WIDTH, HEIGHT, area);
+
+    $display("Calculated volume (LENGTH * DEPTH * HEIGHT) = (%0d * %0d * %0d) = %0d", LENGTH, DEPTH, HEIGHT, volume);
+
+  end
+endmodule
