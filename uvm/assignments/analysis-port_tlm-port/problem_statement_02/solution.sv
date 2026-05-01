c4df3076b6c159b59e925a92299ea1bbc111fedc
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-05-01 19:24:29 +0600

    Update solution.sv

--- uvm/assignments/analysis-port_tlm-port/problem_statement_02/solution.sv
+++ uvm/assignments/analysis-port_tlm-port/problem_statement_02/solution.sv
@@ -4,15 +4,15 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-// === Scoreboard Class ===
+// Scoreboard Class 
 class my_scoreboard extends uvm_scoreboard;
   `uvm_component_utils(my_scoreboard)
 
-// === Separate analysis implementation ports for two monitor streams ===
+  // Separate analysis implementation ports for two monitor streams 
   uvm_analysis_imp_mon1#(my_transaction, my_scoreboard)imp1;
   uvm_analysis_imp_mon2#(my_transaction, my_scoreboard)imp2;
   
-// === Queue to store received transactions ===
+  // Queue to store received transactions 
   my_transaction collected_transaction [$];
   
   function new (string name, uvm_component parent);
@@ -21,13 +21,13 @@ class my_scoreboard extends uvm_scoreboard;
     imp2 = new("imp2", this);
   endfunction
 
-// === Write function for monitor 1 stream ===
+  // Write function for monitor 1 stream 
   function void write_mon1 (my_transaction tr);
     `uvm_info("Scoreboard", $sformatf("Recieved transaction from Monitor1 :%s", tr.convert2string()), UVM_LOW);
     collected_transaction.push_back(tr);
   endfunction
 
-// === Write function for monitor 2 stream ===
+  // Write function for monitor 2 stream
   function void write_mon2 (my_transaction tr);
     `uvm_info("Scoreboard", $sformatf("Recieved transaction from Monitor2 :%s", tr.convert2string()), UVM_LOW);
     collected_transaction.push_back(tr);
