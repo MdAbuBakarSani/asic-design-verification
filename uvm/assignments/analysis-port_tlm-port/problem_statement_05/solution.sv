c1216a0d0aab9b35dfb02857766d107fa2f8e22b
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-05-01 19:30:51 +0600

    Refactor comments for clarity in solution.sv

--- uvm/assignments/analysis-port_tlm-port/problem_statement_05/solution.sv
+++ uvm/assignments/analysis-port_tlm-port/problem_statement_05/solution.sv
@@ -4,7 +4,7 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-// === Sequencer Class ===
+// Sequencer Class 
 class my_sequencer extends uvm_sequencer #(my_transaction);
   `uvm_component_utils(my_sequencer)
 
@@ -16,11 +16,11 @@ class my_sequencer extends uvm_sequencer #(my_transaction);
   endfunction
 endclass
 
-// === Driver Class ===
+// Driver Class 
 class my_driver extends uvm_driver #(my_transaction);
   `uvm_component_utils(my_driver)
 
-// === Will be connected to sequencer.put_port ===
+  // Will be connected to sequencer.put_port
   uvm_blocking_put_imp #(my_transaction, my_driver) put_imp;
 
   function new(string name, uvm_component parent);
@@ -30,12 +30,12 @@ class my_driver extends uvm_driver #(my_transaction);
     virtual task put(my_transaction tr);
       `uvm_info("Driver", $sformatf("Driving transaction: %s", tr.convert2string()), UVM_LOW)
       
-// === Drive DUT here ===
+      // Drive DUT here 
  
     endtask
 endclass
 
-// === Sequence Class ===
+// Sequence Class 
 class my_sequence extends uvm_sequence #(my_transaction);
   `uvm_object_utils(my_sequence)
   `uvm_declare_p_sequencer(my_sequencer)
@@ -50,7 +50,7 @@ class my_sequence extends uvm_sequence #(my_transaction);
       req = my_transaction::type_id::create("req");
       assert(req.randomize());
       
-// === Call put() on the sequencer's port  ===  
+      // Call put() on the sequencer's port  
       `uvm_info("Sequence", $sformatf("Sending item: %s", req.convert2string()), UVM_LOW)
       p_sequencer.put_port.put(req);
     end
