df4aa40fe9a869c8b2905945b5db35a606172659
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-05-01 19:38:41 +0600

    Update comments for clarity in solution.sv

--- uvm/assignments/analysis-port_tlm-port/problem_statement_09/solution.sv
+++ uvm/assignments/analysis-port_tlm-port/problem_statement_09/solution.sv
@@ -7,15 +7,15 @@ import uvm_pkg::*;
 class coverage_component extends uvm_component;
   `uvm_component_utils(coverage_component)
 
-// === Analysis export used to receive transactions from outside this component ===
+  // Analysis export used to receive transactions from outside this component 
   uvm_analysis_export #(my_transaction) analysis_export;
   
-// === Internal analysis implementation used to handle received transactions ===
+  // Internal analysis implementation used to handle received transactions 
   uvm_analysis_imp #(my_transaction, coverage_component) imp;
 
-// === Covergroup used for coverage collection ===
-  covergroup cg;
-     // === coverpoint logic ===
+  // Covergroup used for coverage collection 
+  covergroup cg; 
+    // coverpoint logic 
   endgroup
 
   function new(string name, uvm_component parent);
@@ -25,14 +25,13 @@ class coverage_component extends uvm_component;
     cg = new();
   endfunction
 
-// === Connect phase ===
+  // Connect phase 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     analysis_export.connect(imp);     // Connects export to internal analysis implementation
   endfunction
 
-
-// === This function is called when a transaction arrives through analysis_export ===
+  // This function is called when a transaction arrives through analysis_export 
   function void write(my_transaction tr);
     cg.sample();    // coverage logic
   endfunction
