c5d90e3cc6bd21fe0d6965093f759e9a42c58a47
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-05-01 19:21:47 +0600

    Update solution.sv

--- uvm/assignments/analysis-port_tlm-port/problem_statement_01/solution.sv
+++ uvm/assignments/analysis-port_tlm-port/problem_statement_01/solution.sv
@@ -4,11 +4,11 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-// === Monitor Class ===
+// Monitor Class 
 class my_monitor extends uvm_monitor;
   `uvm_component_utils(my_monitor)
   
-// === Analysis port used to send transactions to other components such as scoreboard ===
+  // Analysis port used to send transactions to other components such as scoreboard 
   uvm_analysis_port#(my_transaction)mon_ap;
   
   function new (string name, uvm_component parent);
@@ -16,7 +16,7 @@ class my_monitor extends uvm_monitor;
     mon_ap = new("mon_ap", this);
   endfunction
 
-// === Run phase: continuously generates/sends observed transactions ===
+  // Run phase: continuously generates/sends observed transactions
   task run_phase (uvm_phase phase);
     forever begin
       my_transaction tr;
@@ -26,14 +26,14 @@ class my_monitor extends uvm_monitor;
   endtask
 endclass
 
-// === Scoreboard Class ===
+// Scoreboard Class 
 class my_scoreboard extends uvm_scoreboard;
   `uvm_component_utils(my_scoreboard)
 
-// === Analysis implementation port used to receive transactions from monitor ===
+  // Analysis implementation port used to receive transactions from monitor 
   uvm_analysis_imp#(my_transaction, my_scoreboard)imp;
 
-// === Queue used to store received transactions ===
+  // Queue used to store received transactions 
   my_transaction collected_transaction [$];
   
   function new (string name, uvm_component parent);
@@ -41,7 +41,7 @@ class my_scoreboard extends uvm_scoreboard;
     imp = new("imp", this);
   endfunction
 
-// === This function is automatically called when monitor writes a transaction ===
+  // This function is automatically called when monitor writes a transaction 
   function void write (my_transaction tr);
     `uvm_info("Scoreboard", $sformatf("Recieved transaction :%s", tr.convert2string()), UVM_LOW);   // Prints the received transaction
     collected_transaction.push_back(tr);    // Stores transaction in queue
