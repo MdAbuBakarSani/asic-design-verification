58ab85c3729685d4be8a14faa943c6e09126a198
MdAbuBakarSani <mdabubakarsani656@gmail.com>
2026-05-01 21:15:36 +0600

    Refactor comments in bus_scoreboard class

--- uvm/assignments/uvm-phases_active-agent_passive-agent/problem_statement_05/solution.sv
+++ uvm/assignments/uvm-phases_active-agent_passive-agent/problem_statement_05/solution.sv
@@ -1,18 +1,17 @@
-
 // Code your testbench here
 // or browse Examples
 
 `include "uvm_macros.svh"   
 import uvm_pkg::*; 
 
-// === Scoreboard Class ===
+// Scoreboard Class 
 class bus_scoreboard extends uvm_scoreboard; 
   `uvm_component_utils(bus_scoreboard) 
 
-// === Analysis implementation port used to receive transactions from the monitor ===
+  // Analysis implementation port used to receive transactions from the monitor 
   uvm_analysis_imp#(bus_transaction, bus_scoreboard)mon_ap; 
 
-// === Queue to store transactions until check_phase starts ===
+  // Queue to store transactions until check_phase starts 
   bus_transaction collected_transaction [$]; 
 
   function new (string name, uvm_component parent); 
@@ -20,17 +19,17 @@ class bus_scoreboard extends uvm_scoreboard;
     mon_ap = new("mon_ap", this); 
   endfunction 
 
-// === This function is called whenever the monitor sends a transaction ===
+  // This function is called whenever the monitor sends a transaction
   function void write (bus_transaction tr); 
 
-// === Logs that transaction was received ===
+    // Logs that transaction was received 
     `uvm_info ("Scoreboard", $sformatf("Received transaction in run phase: %s", tr.convert2string()), UVM_LOW) 
 
-// === Stores transaction for checking later ===
+    // Stores transaction for checking later 
     collected_transaction.push_back(tr); 
   endfunction 
 
-// === Check Phase ===
+  // Check Phase 
   function void check_phase (uvm_phase phase); 
     super.check_phase(phase); 
 
